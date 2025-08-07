f2f8beb3316b1e6c72eae4cb29086246d7914e75
Daniel Reti <daniel.reti@risingstack.com>
2025-08-07 14:52:01 +0200

    add auth

--- config/config.exs
+++ config/config.exs
@@ -7,6 +7,19 @@
 # General application configuration
 import Config
 
+config :elixir_playground, :scopes,
+  user: [
+    default: true,
+    module: ElixirPlayground.Accounts.Scope,
+    assign_key: :current_scope,
+    access_path: [:user, :id],
+    schema_key: :user_id,
+    schema_type: :id,
+    schema_table: :users,
+    test_data_fixture: ElixirPlayground.AccountsFixtures,
+    test_setup_helper: :register_and_log_in_user
+  ]
+
 config :elixir_playground,
   ecto_repos: [ElixirPlayground.Repo],
   generators: [timestamp_type: :utc_datetime]
